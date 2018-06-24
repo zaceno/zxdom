c5ec07d8ac9cee2d5f20ab0bb2d6ac08a8ffbfc0
Zacharias Enochsson <zache@axis.com>
2018-06-24 22:12:21 +0200

    make docs work

--- docs/assets/runnable-example.js
+++ docs/assets/runnable-example.js
@@ -192,31 +192,37 @@
 
     class RunnableExample extends HTMLElement {
         runCode () {
-            const code = this._code;
+            const code = this.textContent;
             Function(
                 'h', 'mount', 'define', 'update', 'make', 'patch',
                 code
             )(h, mount, define, update, make, patch);
         }
         connectedCallback () {
-            this._code = this.textContent;
-            const shadow = this.attachShadow({mode: 'open'});
-            shadow.appendChild(make(
-                h('style', {}, `
-                .example {
-                    border: 1px black solid;
-                }
-                .code {
-                    border-bottom: 1px black solid;
-                }
-            `)
-            ));
-            shadow.appendChild(make(
-                h('div', {class: 'example'}, [
-                    h('div', {class: 'code'}, [this._code]),
-                    h('button', {onclick: _ => this.runCode()}, 'Run')
-                ])
-            ));
+            //to let the syntax highlighter do its work first
+            setTimeout(_ => {
+                const shadow = this.attachShadow({mode: 'open'});
+                shadow.appendChild(make(
+                    h('style', {}, `
+                    @import url('./assets/prism.css');
+                    .runnable {
+                        border: 1px black solid;
+                    }
+                    .code {
+                        border-bottom: 1px black solid;
+                    }
+                `)
+                ));
+                shadow.appendChild(make(
+                    h('div', {class: 'runnable',}, [
+                        h('div', {
+                            class: 'code',
+                            oncreate: el => el.innerHTML = this.innerHTML
+                        }),
+                        h('button', {onclick: _ => this.runCode()}, 'Run')
+                    ])
+                ));
+            }, 0);
         }
     }
 
--- docs/index.md
+++ docs/index.md
@@ -4,7 +4,7 @@ layout: page
 
 You use the `h` function to generate a virtual dom tree: a representation of what you'd like the DOM to look like.
 
-```
+```js
 h('div', {style: 'border: 1px red solid;'}, [
     h('p', {}, 'Hello world'),
     h('button', {onclick: _ => alert('hi!')}, 'Push me')
@@ -13,7 +13,7 @@ h('div', {style: 'border: 1px red solid;'}, [
 
 Generates a virtual node which basically looks like this:
 
-```
+```js
 {
     type: 'div',
     attributes: {
@@ -40,23 +40,29 @@ Generates a virtual node which basically looks like this:
 }
 ```
 
-In order to turn *virtual* dom nodes into *real* dome nodes, use the `make` function. The resulting element can then be mounted 
-somewhere in the real dom:
+In order to turn *virtual* dom nodes into *real* dome nodes, use the `make` function. `make` turns a virtual node such as the above into a dom element equivalent to what this HTML would produce:
+
+```html
+<div style="border: 1px red solid;">
+    <p>Hello world</p>
+    <button onclick="_ => alert('hi')">Push me</button>
+</div>
+```
+
+The resulting element can then be mounted somewhere in the real dom:
 
 
-<runnable-example><pre><code>
-const virtualNode = h('div', {style: 'border: 1px red solid;'}, [
-    h('p', {}, 'Hello world'),
-    h('button', {onclick: _ => alert('hi!')}, 'Push me')
-])
-
-const elem = make(virtualNode)
-
-document.querySelector('#example-make').appendChild(elem)
+<runnable-example><pre><code class="lang-js">
+document.querySelector('#example-make').appendChild(
+    make(
+        h('div', {style: 'border: 1px red solid;'}, [
+            h('p', {}, 'Hello world'),
+            h('button', {onclick: _ => alert('hi!')}, 'Push me')
+        ])
+    )
+)
 </code></pre></runnable-example>
 
-
-<div id="example-make">
-        Example will show here when run
-</div>
+Example will show here when run:
+<div id="example-make"></div>
 
